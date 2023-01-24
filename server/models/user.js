49fc5ce8038f91f0bc6992b9b5d115726081256d
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-01-23 21:13:59 -0800

    added user model with bcrypt hashing and password comparison for logon

---
+++ server/models/user.js
@@ -0,0 +1,50 @@
+const { Schema, model} = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema ({
+    email:{
+        type: String,
+        required: true,
+        unique:true,
+        match:[/.+@.+\..+/, 'Must match an email address!'],
+    },
+    photo:{
+        type: String,
+        required: false
+    }, 
+    username:{
+        type: String,
+        required: true,
+        unique:true,
+        trim: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 10,
+    },
+    online:{
+        type:Boolean
+    },
+    
+});
+
+//Hashing the user's password for security
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// Function to check user password on sign in with bcrypt password
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+const User = model('User', userSchema)
+
+module.exports = User;
