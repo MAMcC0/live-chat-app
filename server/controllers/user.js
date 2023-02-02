041fe77b91541f5dd7d449452c0a962dab1ae1bd
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-02-02 09:17:34 -0800

    added controler files and routes for login and create using  signToken for auth

---
+++ server/controllers/user.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../models/user.js');
+const {signToken}= require('../utils/auth');
+
+//Create user
+router.post('/signup', auth, async (req,res) => {
+    try {
+        User.findOne(
+            {username: req.body.email}
+        ).then((user) => {
+            if(user) {
+                return res.status(400).json({message: "Username already exists"});
+
+            } 
+        })
+
+        User.findOne(
+            {email: req.body.email}
+        ).then((user) => {
+            if(user) {
+                return res.status(400).json({message: "An account with that email already exsists"})
+            } else {
+                const userData = User.create(
+                    { username: req.body.username,
+                      email: req.body.email,
+                      password: req.body.password,
+                    //potentially need to add avatar default in model or est here  
+                    }
+                    );
+            }
+        signToken({email, username});
+        })
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+//route to log in a user
+router.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const userData = await User.findOne({email}).then((user) => {
+            if(!user) {
+                return res.status(404).json({ emailnotfound: "Email not found"})
+            }
+
+            bcrypt.compare(password, user.password).then((isAMatch) => {
+                if (isAMatch){
+                    signToken();
+                } else {
+                    return res.status(400).json({message: "Password Incorrect"});
+                }
+            });
+        })
+    } catch (err){
+        res.status(400).json(err);
+    }
+})
+
